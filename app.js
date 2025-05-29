e7c57d8db440ac559a4c1f8fde0ee9c26770570e
Brandon <brandon.lunaj@gmail.com>
2025-05-29 02:17:31 -0400

    fixed some things throughout this project

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send("hello!")
-})
+    res.sendFile(path.join(__dirname, 'public', 'index.html' , 'main.css'));
+});
 
 app.listen(PORT, () => {
     console.log(`Currently listening on port ${PORT}`);
