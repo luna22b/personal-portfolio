129078e71771a31bdd730e8d83d0b1ad04228894
Brandon Luna <brandon.lunaj@gmail.com>
2025-05-30 01:38:25 -0400

    added ejs folders, useability throughout links and paths

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('index'); 
+});
+
+app.get('/about', (req, res) => {    
+    res.render('about');
+});
+
+app.get('/contacts', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/skills', (req, res) => {  
+    res.render('skills');
+});
+
+app.get('/projects', (req, res) => {  
+    res.render('projects');
 });
 
 app.listen(PORT, () => {
     console.log(`Currently listening on port ${PORT}`);
-})
+});
